kahle23/baibaopy

[_fix_] html2xlsx 列宽重构为基础网格列计划：修复多表同sheet列宽比例失真
- 问题：多张表格共用同一套物理列宽，后渲染表格继承首表宽度（如签署栏 38/38/24 被商品表 18/9/10 覆盖），行高估算也未计字号缩放
- 重构为"基础网格列计划"：sheet 用 base_grid_cols(默认24) 根等宽物理列，每张表按自身百分比（profile 指定 > 首行 width% > 均分）映射到基础列段并以合并单元格组合，各表比例互不干扰
- 行高估算按本表各列段真实字符宽度并引入字号缩放系数（11pt/单元格字号），小字号不再高估行数
- 多栏容器与整行文本块同步改为按基础列段合并；测试全部改写为列计划语义（8/8 过）

src/baibao/oss/__init__.py
@@ -1,0 +1,95 @@
+"""
+对象存储模块。
+
+基于 pykunlun 的 :class:`~pykunlun.oss.OssClient` / :class:`~pykunlun.oss.OssManager`
+门面抽象，提供阿里云 OSS 实现，以及模块级默认管理器实例 :data:`oss_mgr`：
+用户直接拿它按名称注册 client、经便捷方法操作对象存储。
+
+本地目录实现复用 pykunlun 内置的 :class:`~pykunlun.oss.LocalOssClient`（零依赖，
+本地开发/单测默认）；阿里云实现 :class:`AliyunOssClient` 基于 oss2 SDK，
+未安装时首次使用自动 ``pip install oss2``。
+
+实例统一收敛在 :data:`oss_mgr`（:class:`~pykunlun.oss.OssManager`）：按 ``name``
+别名管理多个不同配置的实例，所有操作均可经管理器按名称转发。
+首次按名称取实例时触发配置加载：注册内置实现类，并从 ``.baibao/oss.config``
+（当前目录或用户目录）读取各实例配置，格式为 ``{实例名: OssCfg 字段映射}``::
+
+    {
+      "default":     {"oss_type": "local", "bucket": "demo",
+                       "storage_options": {"base_dir": "D:/data/oss"}},
+      "aliyun-prod": {"oss_type": "aliyun", "bucket": "my-bucket",
+                       "region": "cn-hangzhou", "prefix": "reports/",
+                       "access_key_id": "AK", "access_key_secret": "SK"}
+    }
+
+典型用法::
+
+    from baibao.oss import oss_mgr, AliyunOssClient, OssCfg
+
+    # 经配置文件（.baibao/oss.config）或代码注册实例
+    oss_mgr.register("prod", AliyunOssClient(OssCfg(
+        oss_type='aliyun', bucket='my-bucket', region='cn-hangzhou',
+        access_key_id='AK', access_key_secret='SK')))
+
+    # 按别名操作（省略 name 时用默认名 "default"）
+    oss_mgr.put_object('reports/2026.txt', 'hello', name="prod")
+    text = oss_mgr.get_object_text('reports/2026.txt', name="prod")
+    url  = oss_mgr.presigned_url('reports/2026.txt', expires=600, name="prod")
+"""
+
+import json
+from typing import Any
+
+from pykunlun.oss import LocalOssClient, ObjectStat, OssCfg, OssClient, OssManager
+from pykunlun.util import ResolveType, fileutil, logutil
+
+from .aliyun_client import AliyunOssClient
+
+log = logutil.getLogger(__name__)
+
+#: 配置加载幂等标记：只加载一次，失败也不重试（避免每次 get_client 都刷告警）
+_config_loaded = False
+
+
+def _config_loader(manager: OssManager, name: str) -> None:
+    """
+    配置加载器：注册实现类并从 ``.baibao/oss.config`` 加载各实例配置，只执行一次。
+
+    Args:
+        manager: 发起加载的管理器（实现类与实例均注册到它身上）。
+        name: 触发加载的实例名（本加载器为全量加载，不按 name 区分）。
+    """
+    global _config_loaded
+    if _config_loaded:
+        return
+    _config_loaded = True
+
+    # 注册实现类（local 来自 pykunlun 内置，aliyun 来自本包）
+    manager.register_client_class(LocalOssClient)
+    manager.register_client_class(AliyunOssClient)
+
+    # 从配置文件加载实例（{实例名: OssCfg 字段映射}）
+    # 捕获具体异常降级告警：配置缺文件/格式坏/字段错均不阻断后续手动 register
+    try:
+        content = fileutil.read_text(".baibao/oss.config",
+                                     search_dirs=[ResolveType.CURRENT, ResolveType.USER])
+        config: dict[str, dict[str, Any]] = json.loads(content)
+        for n, cfg_data in config.items():
+            manager.register(n, OssCfg(**cfg_data))
+    except (OSError, ValueError, TypeError) as e:
+        log.warning("Failed to load oss config: %s", e)
+
+
+#: 模块级默认管理器实例：按名称（别名）管理各对象存储客户端实例，
+#: 首次按名称取实例时自动加载 .baibao/oss.config。
+oss_mgr: OssManager = OssManager(config_loader=_config_loader)
+
+__all__ = [
+    'AliyunOssClient',
+    'LocalOssClient',
+    'ObjectStat',
+    'OssCfg',
+    'OssClient',
+    'OssManager',
+    'oss_mgr',
+]

src/baibao/render/convert/html2xlsx/style_parser.py
@@ -1,0 +1,240 @@
+"""
+html2xlsx 样式解析器。
+
+回答"一个 HTML 节点最终应用什么单元格样式"，三层合成：
+    1. 标签语义默认（th 居中加粗、b/strong 加粗、i/em 斜体等）；
+    2. profile 的 class 映射（支持 ``.cls``/``tag``/``.scope .cls`` 选择器，按特异性升序合并）；
+    3. 行内 ``style`` 属性（CSS 白名单子集，优先级最高）。
+解析失败的降级一律记入 warnings，不做静默丢弃。
+"""
+
+from __future__ import annotations
+
+import re
+from dataclasses import replace
+from typing import Any
+
+from baibao.office.excel import BorderStyle, CellStyle
+
+from .model import Html2XlsxProfile
+
+# region ======== 常量与正则 ========
+# px → pt（CSS 96dpi 约定）
+_PX_TO_PT = 0.75
+
+# 行内样式属性的白名单解析（白名单之外的属性记"忽略"警告）
+_COLOR_RE = re.compile(r"^(#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})|[a-zA-Z]+)$")
+_BORDER_RE = re.compile(r"^\s*(\d+)px\s+\w+\s+(#\w+|[a-zA-Z]+)\s*$")
+_BORDER_SIDE_PROPS = ("border-top", "border-bottom", "border-left", "border-right")
+# endregion
+
+
+# region ======== 标签默认与 class 匹配 ========
+def _tag_default(name: str) -> CellStyle | None:
+    """
+    标签语义默认样式（th 表头居中加粗、行内强调、标题加粗），解析期与 class 映射合成。
+    """
+    if name == "th":
+        return CellStyle(bold=True, halign="center", valign="center")
+    if name in ("b", "strong"):
+        return CellStyle(bold=True)
+    if name in ("i", "em"):
+        return CellStyle(italic=True)
+    if name == "u":
+        return CellStyle(underline=True)
+    if name in ("h1", "h2", "h3"):
+        return CellStyle(bold=True)
+    return None
+
+
+def _classes_of(node: Any) -> list[str]:
+    """
+    取节点的 class 列表，文本节点返回空表。
+
+    兼容 beautifulsoup4 新旧版本：4.13+ 的多值属性返回列表，旧版返回空格分隔字符串。
+    """
+    if not hasattr(node, "get"):
+        return []
+    value = node.get("class")
+    if value is None:
+        return []
+    if isinstance(value, str):
+        return value.split()
+    return [str(item) for item in value]
+
+
+def _first_class(node: Any) -> str | None:
+    """
+    取节点第一个 class，无 class 返回 None。
+    """
+    classes = _classes_of(node) if node is not None else []
+    return classes[0] if classes else None
+
+
+def _ancestor_has_class(node: Any, cls: str, root: Any) -> bool:
+    """
+    判断 node 是否有带指定 class 的祖先（不越过 root）。
+    """
+    parent = node.parent
+    while parent is not None and parent is not root:
+        if cls in _classes_of(parent):
+            return True
+        parent = parent.parent
+    return False
+
+
+def _matching_class_styles(node: Any, root: Any, profile: Html2XlsxProfile) -> list[CellStyle]:
+    """
+    收集命中的 class 映射，按特异性（作用域深度）升序返回。
+    """
+    classes = _classes_of(node)
+    hits: list[tuple[int, CellStyle]] = []
+    for key, style in profile.class_styles.items():
+        tokens = key.split()
+        if not tokens:
+            continue
+        target = tokens[-1]
+        is_class = target.startswith(".")
+        matched = (is_class and target[1:] in classes) or (not is_class and node.name == target)
+        if not matched:
+            continue
+        scope_ok = all(
+            _ancestor_has_class(node, tok[1:], root) if tok.startswith(".") else True
+            for tok in tokens[:-1]
+        )
+        if scope_ok:
+            hits.append((len(tokens), style))
+    hits.sort(key=lambda item: item[0])
+    return [style for _, style in hits]
+# endregion
+
+
+# region ======== 行内样式解析 ========
+def _normalize_color(value: str) -> str | None:
+    """
+    把 #RGB/#RRGGBB/常见英文色名规整为 openpyxl 的 ARGB 字符串，未知值返回 None。
+    """
+    value = value.strip()
+    if not _COLOR_RE.match(value):
+        return None
+    named = {
+        "black": "000000", "white": "FFFFFF", "red": "FF0000", "green": "008000",
+        "blue": "0000FF", "gray": "808080", "grey": "808080", "yellow": "FFFF00",
+        "orange": "FFA500", "#333": "333333", "#fff": "FFFFFF", "#000": "000000",
+    }
+    hex6 = named.get(value.lower())
+    if hex6 is None and value.startswith("#"):
+        raw = value[1:]
+        hex6 = raw if len(raw) == 6 else "".join(ch * 2 for ch in raw)
+    if hex6 is None:
+        return None
+    return "FF" + hex6.upper()
+
+
+def _parse_inline_style(node: Any, style: CellStyle, warnings: list[str]) -> CellStyle:
+    """
+    解析行内 ``style`` 属性进样式，行内优先级最高。
+
+    只认白名单子集：认识的属性值非法记"无法解析"警告，白名单之外的属性记"忽略"
+    警告，不做静默丢弃。
+    """
+    raw = node.get("style") if hasattr(node, "get") else None
+    if not raw:
+        return style
+    for decl in raw.split(";"):
+        if ":" not in decl:
+            continue
+        prop, _, value = decl.partition(":")
+        prop = prop.strip().lower()
+        value = value.strip()
+        if not prop or not value:
+            continue
+
+        # 对齐类
+        if prop == "text-align":
+            if value in ("left", "right", "center"):
+                style = replace(style, halign=value)
+            else:
+                warnings.append(f"无法解析 text-align: {value}")
+        elif prop == "vertical-align":
+            if value in ("top", "middle", "bottom"):
+                style = replace(style, valign="center" if value == "middle" else value)
+            else:
+                warnings.append(f"无法解析 vertical-align: {value}")
+
+        # 字体类
+        elif prop == "font-weight":
+            if value in ("bold", "700", "800", "900"):
+                style = replace(style, bold=True)
+            else:
+                warnings.append(f"无法解析 font-weight: {value}（仅支持 bold/700/800/900）")
+        elif prop == "font-size":
+            if value.endswith("px"):
+                try:
+                    style = replace(style, font_size_pt=float(value[:-2]) * _PX_TO_PT)
+                except ValueError:
+                    warnings.append(f"无法解析 font-size: {value}")
+            else:
+                warnings.append(f"无法解析 font-size: {value}（仅支持 px）")
+        elif prop == "color":
+            if argb := _normalize_color(value):
+                style = replace(style, font_color=argb)
+            else:
+                warnings.append(f"无法解析颜色: {value}（支持 #hex 与常见英文色名）")
+
+        # 背景与布局类
+        elif prop in ("background", "background-color"):
+            if argb := _normalize_color(value):
+                style = replace(style, fill_color=argb)
+            else:
+                warnings.append(f"无法解析颜色: {value}（支持 #hex 与常见英文色名）")
+        elif prop == "white-space":
+            if value == "pre-line":
+                style = replace(style, wrap_text=True)
+            else:
+                warnings.append(f"无法解析 white-space: {value}（仅支持 pre-line）")
+        elif prop == "width":
+            if value.endswith("%"):
+                try:
+                    style = replace(style, width_percent=float(value[:-1]))
+                except ValueError:
+                    warnings.append(f"无法解析 width: {value}")
+            else:
+                warnings.append(f"无法解析 width: {value}（仅支持百分比）")
+        elif prop == "border" or prop in _BORDER_SIDE_PROPS:
+            style = _apply_border_decl(prop, value, style, warnings)
+
+        # 白名单之外：无 Excel 对应物，记警告忽略
+        else:
+            warnings.append(f"忽略无 Excel 对应物的 CSS 属性: {prop}")
+    return style
+
+
+def _apply_border_decl(prop: str, value: str, style: CellStyle, warnings: list[str]) -> CellStyle:
+    """
+    解析 ``border[-position]`` 声明（含 ``none``）到对应边。
+
+    ``none``/``0`` 显式去边框；其余按 ``Npx <样式> <颜色>`` 解析，语序不符记警告。
+    """
+    sides = list(_BORDER_SIDE_PROPS) if prop == "border" else [prop]
+    if value.strip() in ("none", "0"):
+        return replace(style, **{f"border_{side[7:]}": BorderStyle(style=None) for side in sides})
+    match = _BORDER_RE.match(value)
+    if not match:
+        warnings.append(f"无法解析 {prop}: {value}（支持 'Npx solid <颜色>' 形式）")
+        return style
+    color = _normalize_color(match.group(2)) or "FF000000"
+    side = BorderStyle(style="thin", color=color)
+    new = {f"border_{s}": side for s in sides}
+    return replace(style, **new)
+
+
+def _resolve_style(node: Any, root: Any, profile: Html2XlsxProfile, warnings: list[str]) -> CellStyle:
+    """
+    三层合成：标签默认 → class 映射 → 行内样式。
+    """
+    style = _tag_default(node.name) or CellStyle()
+    for hit in _matching_class_styles(node, root, profile):
+        style = style.merge(hit)
+    return _parse_inline_style(node, style, warnings)
+# endregion

kahle23/pybaibao

src/baibao/oss/__init__.py
@@ -1,0 +1,95 @@
+"""
+对象存储模块。
+
+基于 pykunlun 的 :class:`~pykunlun.oss.OssClient` / :class:`~pykunlun.oss.OssManager`
+门面抽象，提供阿里云 OSS 实现，以及模块级默认管理器实例 :data:`oss_mgr`：
+用户直接拿它按名称注册 client、经便捷方法操作对象存储。
+
+本地目录实现复用 pykunlun 内置的 :class:`~pykunlun.oss.LocalOssClient`（零依赖，
+本地开发/单测默认）；阿里云实现 :class:`AliyunOssClient` 基于 oss2 SDK，
+未安装时首次使用自动 ``pip install oss2``。
+
+实例统一收敛在 :data:`oss_mgr`（:class:`~pykunlun.oss.OssManager`）：按 ``name``
+别名管理多个不同配置的实例，所有操作均可经管理器按名称转发。
+首次按名称取实例时触发配置加载：注册内置实现类，并从 ``.baibao/oss.config``
+（当前目录或用户目录）读取各实例配置，格式为 ``{实例名: OssCfg 字段映射}``::
+
+    {
+      "default":     {"oss_type": "local", "bucket": "demo",
+                       "storage_options": {"base_dir": "D:/data/oss"}},
+      "aliyun-prod": {"oss_type": "aliyun", "bucket": "my-bucket",
+                       "region": "cn-hangzhou", "prefix": "reports/",
+                       "access_key_id": "AK", "access_key_secret": "SK"}
+    }
+
+典型用法::
+
+    from baibao.oss import oss_mgr, AliyunOssClient, OssCfg
+
+    # 经配置文件（.baibao/oss.config）或代码注册实例
+    oss_mgr.register_client("prod", AliyunOssClient(OssCfg(
+        oss_type='aliyun', bucket='my-bucket', region='cn-hangzhou',
+        access_key_id='AK', access_key_secret='SK')))
+
+    # 按别名操作（省略 name 时用默认名 "default"）
+    oss_mgr.put_object('reports/2026.txt', 'hello', name="prod")
+    text = oss_mgr.get_object_text('reports/2026.txt', name="prod")
+    url  = oss_mgr.presigned_url('reports/2026.txt', expires=600, name="prod")
+"""
+
+import json
+from typing import Any
+
+from pykunlun.oss import LocalOssClient, ObjectStat, OssCfg, OssClient, OssManager
+from pykunlun.util import ResolveType, fileutil, logutil
+
+from .aliyun_client import AliyunOssClient
+
+log = logutil.getLogger(__name__)
+
+#: 配置加载幂等标记：只加载一次，失败也不重试（避免每次 get_client 都刷告警）
+_config_loaded = False
+
+
+def _config_loader(manager: OssManager, name: str) -> None:
+    """
+    配置加载器：注册实现类并从 ``.baibao/oss.config`` 加载各实例配置，只执行一次。
+
+    Args:
+        manager: 发起加载的管理器（实现类与实例均注册到它身上）。
+        name: 触发加载的实例名（本加载器为全量加载，不按 name 区分）。
+    """
+    global _config_loaded
+    if _config_loaded:
+        return
+    _config_loaded = True
+
+    # 注册实现类（local 来自 pykunlun 内置，aliyun 来自本包）
+    manager.register_client_class(LocalOssClient)
+    manager.register_client_class(AliyunOssClient)
+
+    # 从配置文件加载实例（{实例名: OssCfg 字段映射}）
+    # 捕获具体异常降级告警：配置缺文件/格式坏/字段错均不阻断后续手动 register_client
+    try:
+        content = fileutil.read_text(".baibao/oss.config",
+                                     search_dirs=[ResolveType.CURRENT, ResolveType.USER])
+        config: dict[str, dict[str, Any]] = json.loads(content)
+        for n, cfg_data in config.items():
+            manager.register_client(n, OssCfg(**cfg_data))
+    except (OSError, ValueError, TypeError) as e:
+        log.warning("Failed to load oss config: %s", e)
+
+
+#: 模块级默认管理器实例：按名称（别名）管理各对象存储客户端实例，
+#: 首次按名称取实例时自动加载 .baibao/oss.config。
+oss_mgr: OssManager = OssManager(config_loader=_config_loader)
+
+__all__ = [
+    'AliyunOssClient',
+    'LocalOssClient',
+    'ObjectStat',
+    'OssCfg',
+    'OssClient',
+    'OssManager',
+    'oss_mgr',
+]

src/baibao/render/convert/html2xlsx/style_parser.py
@@ -1,0 +1,243 @@
+"""
+html2xlsx 样式解析器。
+
+回答"一个 HTML 节点最终应用什么单元格样式"，三层合成：
+    1. 标签语义默认（th 居中加粗、b/strong 加粗、i/em 斜体等）；
+    2. profile 的 class 映射（支持 ``.cls``/``tag``/``.scope .cls`` 选择器，按特异性升序合并）；
+    3. 行内 ``style`` 属性（CSS 白名单子集，优先级最高）。
+解析失败的降级一律记入 warnings，不做静默丢弃。
+"""
+
+from __future__ import annotations
+
+import re
+from dataclasses import replace
+from typing import Any
+
+from baibao.office.excel import BorderStyle, CellStyle
+
+from .model import Html2XlsxProfile
+
+# region ======== 常量与正则 ========
+# px → pt（CSS 96dpi 约定）
+_PX_TO_PT = 0.75
+
+# 行内样式属性的白名单解析（白名单之外的属性记"忽略"警告）
+_COLOR_RE = re.compile(r"^(#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})|[a-zA-Z]+)$")
+_BORDER_RE = re.compile(r"^\s*(\d+)px\s+\w+\s+(#\w+|[a-zA-Z]+)\s*$")
+_BORDER_SIDE_PROPS = ("border-top", "border-bottom", "border-left", "border-right")
+# endregion
+
+
+# region ======== 标签默认与 class 匹配 ========
+def _tag_default(name: str) -> CellStyle | None:
+    """
+    标签语义默认样式（th 表头居中加粗、行内强调、标题加粗），解析期与 class 映射合成。
+    """
+    if name == "th":
+        return CellStyle(bold=True, halign="center", valign="center")
+    if name in ("b", "strong"):
+        return CellStyle(bold=True)
+    if name in ("i", "em"):
+        return CellStyle(italic=True)
+    if name == "u":
+        return CellStyle(underline=True)
+    if name in ("h1", "h2", "h3"):
+        return CellStyle(bold=True)
+    return None
+
+
+def classes_of(node: Any) -> list[str]:
+    """
+    取节点的 class 列表，文本节点返回空表。
+
+    兼容 beautifulsoup4 新旧版本：4.13+ 的多值属性返回列表，旧版返回空格分隔字符串。
+    """
+    if not hasattr(node, "get"):
+        return []
+    value = node.get("class")
+    if value is None:
+        return []
+    if isinstance(value, str):
+        return value.split()
+    return [str(item) for item in value]
+
+
+def first_class(node: Any) -> str | None:
+    """
+    取节点第一个 class，无 class 返回 None。
+    """
+    classes = classes_of(node) if node is not None else []
+    return classes[0] if classes else None
+
+
+def _ancestor_has_class(node: Any, cls: str, root: Any) -> bool:
+    """
+    判断 node 是否有带指定 class 的祖先（不越过 root）。
+    """
+    parent = node.parent
+    while parent is not None and parent is not root:
+        if cls in classes_of(parent):
+            return True
+        parent = parent.parent
+    return False
+
+
+def _matching_class_styles(node: Any, root: Any, profile: Html2XlsxProfile) -> list[CellStyle]:
+    """
+    收集命中的 class 映射，按特异性（作用域深度）升序返回。
+    """
+    classes = classes_of(node)
+    hits: list[tuple[int, CellStyle]] = []
+    for key, style in profile.class_styles.items():
+        tokens = key.split()
+        if not tokens:
+            continue
+        target = tokens[-1]
+        is_class = target.startswith(".")
+        matched = (is_class and target[1:] in classes) or (not is_class and node.name == target)
+        if not matched:
+            continue
+        scope_ok = all(
+            _ancestor_has_class(node, tok[1:], root) if tok.startswith(".") else True
+            for tok in tokens[:-1]
+        )
+        if scope_ok:
+            hits.append((len(tokens), style))
+    hits.sort(key=lambda item: item[0])
+    return [style for _, style in hits]
+# endregion
+
+
+# region ======== 行内样式解析 ========
+def _normalize_color(value: str) -> str | None:
+    """
+    把 #RGB/#RRGGBB/常见英文色名规整为 openpyxl 的 ARGB 字符串，未知值返回 None。
+    """
+    value = value.strip()
+    if not _COLOR_RE.match(value):
+        return None
+    named = {
+        "black": "000000", "white": "FFFFFF", "red": "FF0000", "green": "008000",
+        "blue": "0000FF", "gray": "808080", "grey": "808080", "yellow": "FFFF00",
+        "orange": "FFA500", "#333": "333333", "#fff": "FFFFFF", "#000": "000000",
+    }
+    hex6 = named.get(value.lower())
+    if hex6 is None and value.startswith("#"):
+        raw = value[1:]
+        hex6 = raw if len(raw) == 6 else "".join(ch * 2 for ch in raw)
+    if hex6 is None:
+        return None
+    return "FF" + hex6.upper()
+
+
+def _parse_inline_style(node: Any, style: CellStyle, warnings: list[str]) -> CellStyle:
+    """
+    解析行内 ``style`` 属性进样式，行内优先级最高。
+
+    只认白名单子集：认识的属性值非法记"无法解析"警告，白名单之外的属性记"忽略"
+    警告，不做静默丢弃。
+    """
+    raw = node.get("style") if hasattr(node, "get") else None
+    if not raw:
+        return style
+    for decl in raw.split(";"):
+        if ":" not in decl:
+            continue
+        prop, _, value = decl.partition(":")
+        prop = prop.strip().lower()
+        value = value.strip()
+        if not prop or not value:
+            continue
+
+        # 对齐类
+        if prop == "text-align":
+            if value in ("left", "right", "center"):
+                style = replace(style, halign=value)
+            else:
+                warnings.append(f"无法解析 text-align: {value}")
+        elif prop == "vertical-align":
+            if value in ("top", "middle", "bottom"):
+                style = replace(style, valign="center" if value == "middle" else value)
+            else:
+                warnings.append(f"无法解析 vertical-align: {value}")
+
+        # 字体类
+        elif prop == "font-weight":
+            if value in ("bold", "700", "800", "900"):
+                style = replace(style, bold=True)
+            else:
+                warnings.append(f"无法解析 font-weight: {value}（仅支持 bold/700/800/900）")
+        elif prop == "font-size":
+            if value.endswith("px"):
+                try:
+                    style = replace(style, font_size_pt=float(value[:-2]) * _PX_TO_PT)
+                except ValueError:
+                    warnings.append(f"无法解析 font-size: {value}")
+            else:
+                warnings.append(f"无法解析 font-size: {value}（仅支持 px）")
+        elif prop == "color":
+            if argb := _normalize_color(value):
+                style = replace(style, font_color=argb)
+            else:
+                warnings.append(f"无法解析颜色: {value}（支持 #hex 与常见英文色名）")
+
+        # 背景与布局类
+        elif prop in ("background", "background-color"):
+            if argb := _normalize_color(value):
+                style = replace(style, fill_color=argb)
+            else:
+                warnings.append(f"无法解析颜色: {value}（支持 #hex 与常见英文色名）")
+        elif prop == "white-space":
+            if value == "pre-line":
+                style = replace(style, wrap_text=True)
+            else:
+                warnings.append(f"无法解析 white-space: {value}（仅支持 pre-line）")
+        elif prop == "width":
+            if value.endswith("%"):
+                try:
+                    style = replace(style, width_percent=float(value[:-1]))
+                except ValueError:
+                    warnings.append(f"无法解析 width: {value}")
+            else:
+                warnings.append(f"无法解析 width: {value}（仅支持百分比）")
+        elif prop == "border" or prop in _BORDER_SIDE_PROPS:
+            style = _apply_border_decl(prop, value, style, warnings)
+
+        # 白名单之外：无 Excel 对应物，记警告忽略
+        else:
+            warnings.append(f"忽略无 Excel 对应物的 CSS 属性: {prop}")
+    return style
+
+
+def _apply_border_decl(prop: str, value: str, style: CellStyle, warnings: list[str]) -> CellStyle:
+    """
+    解析 ``border[-position]`` 声明（含 ``none``）到对应边。
+
+    ``none``/``0`` 显式去边框；其余按 ``Npx <样式> <颜色>`` 解析，语序不符记警告。
+    """
+    sides = list(_BORDER_SIDE_PROPS) if prop == "border" else [prop]
+    if value.strip() in ("none", "0"):
+        none_borders: dict[str, Any] = {
+            f"border_{side[7:]}": BorderStyle(style=None) for side in sides
+        }
+        return replace(style, **none_borders)
+    match = _BORDER_RE.match(value)
+    if not match:
+        warnings.append(f"无法解析 {prop}: {value}（支持 'Npx solid <颜色>' 形式）")
+        return style
+    color = _normalize_color(match.group(2)) or "FF000000"
+    side = BorderStyle(style="thin", color=color)
+    borders: dict[str, Any] = {f"border_{s}": side for s in sides}
+    return replace(style, **borders)
+
+
+def resolve_style(node: Any, root: Any, profile: Html2XlsxProfile, warnings: list[str]) -> CellStyle:
+    """
+    三层合成：标签默认 → class 映射 → 行内样式。
+    """
+    style = _tag_default(node.name) or CellStyle()
+    for hit in _matching_class_styles(node, root, profile):
+        style = style.merge(hit)
+    return _parse_inline_style(node, style, warnings)
+# endregion